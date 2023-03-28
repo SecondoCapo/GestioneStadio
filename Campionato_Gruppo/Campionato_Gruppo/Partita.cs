5a15a72dbeed68fe39c19a8f07c488f5325a5048
SecondoCapo <100679260+SecondoCapo@users.noreply.github.com>
2023-03-28 13:08:37 +0200

    Inizio

---
+++ Campionato_Gruppo/Campionato_Gruppo/Partita.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Campionato_Gruppo
+{
+    internal class Partita
+    {
+        public Partita() { }
+        private int goalCasa;
+        public int GoalCasa
+        {
+            get { return goalCasa; }
+            set { goalCasa = value; }
+        }
+        private int goalOspiti;
+        public int GoalOspiti
+        {
+            get => goalOspiti; set {  goalOspiti = value; }
+        }
+        private DateTime data;
+        public DateTime Data
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                data = value;
+            }
+        }
+        private Persona[] arbitri;
+        public Persona[] Arbitri
+        {
+            get { return arbitri; }
+            set { arbitri = value; }
+        }
+    }
+}
